396a6b312de7d3ed4ff0aa2668adcf3e0518e24c
Jinbro <pk0267@naver.com>
2016-04-05 16:41:56 +0000

    first app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KsuLikelion::Application.config.secret_key_base = '4b26b41e6a3a3e1930fc22226c2856134fa976073d105d1c65d430f1c2deee741f4269de8086469e618b4b2d1c22bf24959f00e5e9158b87e0440d32b4eee9b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KsuLikelion::Application.config.session_store :cookie_store, key: '_ksu_likelion_session'
